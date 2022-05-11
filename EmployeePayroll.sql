3d190de1bd0772a96bcb2f16959a479515941a2a
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-11 16:44:46 +0530

    EmployeePayrollTable

---
+++ EmployeePayroll.sql
@@ -0,0 +1,20 @@
+--UC-1=Creat a database
+
+CREATE DATABASE payroll_service;
+ USE payroll_service;
+ Select * FROM employee_payroll;
+
+ --UC2
+
+CREATE TABLE employee_payroll
+(
+id       INT  IDENTITY (1,1) PRIMARY KEY,
+name     VARCHAR(150) NOT NULL,
+salary   BIGINT ,
+start    Date NOT NULL,
+
+);
+SELECT *from employee_payroll;
+ 
+ 
+ 
